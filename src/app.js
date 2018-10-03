d0c900fb3fca7b23dfe69972f6485e956219f3e5
Andres Mena <ballerlikemike23@gmail.com>
2018-10-02 22:56:16 -0400

    Method Binding

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+const obj = {
+  name: 'Vikram',
+  getName() {
+    return this.name
+  }
+}
+
+const getName = obj.getName.bind({ name: 'Andres' })
+
+console.log(getName())
 class IndecisionApp extends React.Component {
   render() {
     const title = 'Indecision'
@@ -41,8 +51,13 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super()
+    this.handleRemoveAll = this.handleRemoveAll.bind(this)
+  }
   handleRemoveAll() {
-    alert('removeAll')
+    console.log(this.props.options)
+    // alert('removeAll')
   }
 
   render() {
