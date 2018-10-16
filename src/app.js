a0f6a2d54316fa2b771614bd5555ee8794467f6e
Andres Mena <ballerlikemike23@gmail.com>
2018-10-15 21:02:54 -0400

    Importing React & ReactDOM

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
-// import subtract, { square, add } from './utils'
+// install -> import -> use
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// console.log('app.js is running!')
-// console.log(square(4))
-// console.log(add(100, 23))
-// console.log(subtract(100, 81))
-
-import isSenior, { isAdult, canDrink } from './person'
-
-console.log(isAdult(17))
-console.log(isAdult(18))
-console.log(isAdult(22))
-
-console.log(canDrink(21))
-console.log(canDrink(18))
-console.log(canDrink(30))
-
-console.log(isSenior(64))
+const template = React.createElement('p', {}, 'testing 123')
+ReactDOM.render(template, document.getElementById('app'))
